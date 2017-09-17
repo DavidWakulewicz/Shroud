bfa1e5be10504f5869d512c5b541697f7ce07d9c
David Wakulewicz <davidwakulewicz@David-PC.localdomain>
2017-09-17 19:04:10 -0400

    Changed starting code, loads a texture

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
 #include <string>
 #include <Windows.h>
@@ -7,18 +8,6 @@ using namespace std;
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//Loads individual image as texture
-SDL_Texture* loadTexture(std::string path);
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -29,92 +18,8 @@ SDL_Renderer* gRenderer = NULL;
 //Current displayed texture
 SDL_Texture* gTexture = NULL;
 
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-		{
-			printf("Warning: Linear texture filtering not enabled!");
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			//Create renderer for window
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-			if (gRenderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if (!(IMG_Init(imgFlags) & imgFlags))
-				{
-					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load PNG texture
-	gTexture = loadTexture("texture.png");
-	if (gTexture == NULL)
-	{
-		printf("Failed to load texture image!\n");
-		success = false;
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Free loaded image
-	SDL_DestroyTexture(gTexture);
-	gTexture = NULL;
-
-	//Destroy window	
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-	gRenderer = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
+//Loads individual image as texture
+SDL_Texture* loadTexture(std::string path);
 
 SDL_Texture* loadTexture(std::string path)
 {
@@ -143,55 +48,88 @@ SDL_Texture* loadTexture(std::string path)
 	return newTexture;
 }
 
-int main(int argc, char* args[])
-{
-	//Start up SDL and create window
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
+int main(int argc, char* args[]) {
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0) {
+		OutputDebugString("SDL Could not be initialized, exiting\n");
+		exit(1);
 	}
-	else
+
+	//Set texture filtering to linear
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		OutputDebugString("Warning: Linear texture filtering not enabled!\n");
+	}
+
+	// Create window
+	// SDL_WINDOW_SHOWN flag is ignored according to document.
+	// Might want to eventually use SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_INPUT_GRABBED
+	// https://wiki.libsdl.org/SDL_CreateWindow
+	gWindow = SDL_CreateWindow("A cool title",
+								SDL_WINDOWPOS_CENTERED,
+								SDL_WINDOWPOS_CENTERED,
+								SCREEN_WIDTH,
+								SCREEN_HEIGHT,
+								SDL_WINDOW_SHOWN); 
+	if (gWindow == NULL) {
+		OutputDebugString("Window could not be created!\n");
+		exit(1);
+	}
+
+	//Create renderer for window
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	if (gRenderer == NULL) {
+		OutputDebugString("Renderer could not be created!\n");
+		exit(1);
+	}
+
+	//Initialize renderer color (renderer, red, green, blue, alpha)
+	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+	// Requires another library 'SDL_Image'
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)) {
+		OutputDebugString("\n\n\n");
+		OutputDebugString(IMG_GetError());
+		OutputDebugString("\n\n\n");
+		exit(2);
+	}
+
+	//Load PNG texture
+	gTexture = loadTexture("texture.png");
+	if (gTexture == NULL)
 	{
-		//Load media
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
+		OutputDebugString("Failed to load texture image!\n");
+		exit(1);
+	}
 
-			//Event handler
-			SDL_Event e;
+	//Main loop flag
+	bool quit = false;
 
-			//While application is running
-			while (!quit)
+	//Event handler
+	SDL_Event e;
+
+	//While application is running
+	while (!quit)
+	{
+		//Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//User requests quit
+			if (e.type == SDL_QUIT)
 			{
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
-
-				//Clear screen
-				SDL_RenderClear(gRenderer);
-
-				//Render texture to screen
-				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
+				quit = true;
 			}
 		}
-	}
 
-	//Free resources and close SDL
-	close();
+		//Clear screen
+		SDL_RenderClear(gRenderer);
+
+		//Render texture to screen
+		SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+		//Update screen
+		SDL_RenderPresent(gRenderer);
+	}
 
 	return 0;
 }
