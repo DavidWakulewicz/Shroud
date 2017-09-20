98ba2820e368b699e6210c0d80c6e5fbd0d0b69f
Dumtard <charles.black90@gmail.com>
2017-09-19 22:16:49 -0400

    Remove windows dependancy, and move code into a class

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,27 +1,106 @@
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <string>
-#include <Windows.h>
-using namespace std;
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+class Game
+{
+public:
+	Game();
+	~Game();
 
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
+	void run();
+	void stop();
+private:
+	bool quit;
 
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
+	//Screen dimension constants
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
 
-//Current displayed texture
-SDL_Texture* gTexture = NULL;
+	//The window we'll be rendering to
+	SDL_Window* window;
+
+	//The window renderer
+	SDL_Renderer* renderer;
+
+	//Current displayed texture
+	SDL_Texture* texture;
+
+	SDL_Texture* loadTexture(std::string path);
+
+	void handleInput(SDL_Event);
+};
+
+Game::Game()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0) {
+		std::cout << "SDL Could not be initialized, exiting" << std::endl;
+		exit(1);
+	}
+
+	//Set texture filtering to linear
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		std::cout << "Warning: Linear texture filtering not enabled!" << std::endl;
+	}
+
+	// Create window
+	// SDL_WINDOW_SHOWN flag is ignored according to document.
+	// Might want to eventually use SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_INPUT_GRABBED
+	// https://wiki.libsdl.org/SDL_CreateWindow
+	window = SDL_CreateWindow("A cool title",
+	                           SDL_WINDOWPOS_CENTERED,
+	                           SDL_WINDOWPOS_CENTERED,
+	                           SCREEN_WIDTH,
+	                           SCREEN_HEIGHT,
+	                           SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		std::cout << "Window could not be created!" << std::endl;
+		exit(1);
+	}
+
+	//Create renderer for window
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL) {
+		std::cout << "Renderer could not be created!" << std::endl;
+		exit(1);
+	}
+
+	//Initialize renderer color (renderer, red, green, blue, alpha)
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+	// Requires another library 'SDL_Image'
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)) {
+		std::cout << "\n\n\n";
+		std::cout << IMG_GetError() << std::endl;
+		std::cout << "\n\n\n";
+		exit(2);
+	}
+
+	//Load PNG texture
+	texture = loadTexture("texture.png");
+	if (texture == NULL)
+	{
+		std::cout << "Failed to load texture image!" << std::endl;
+		exit(1);
+	}
+
+	//Main loop flag
+	quit = false;
+}
+
+Game::~Game()
+{
+	IMG_Quit();
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
 //Loads individual image as texture
-SDL_Texture* loadTexture(std::string path);
-
-SDL_Texture* loadTexture(std::string path)
+SDL_Texture* Game::loadTexture(std::string path)
 {
 	//The final texture
 	SDL_Texture* newTexture = NULL;
@@ -29,16 +108,14 @@ SDL_Texture* loadTexture(std::string path)
 	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
 	if (loadedSurface == NULL)
-	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-	}
+	{ std::cout << "Unable to load image " << path.c_str() << "! SDL_image Error: " << IMG_GetError() << std::endl; }
 	else
 	{
 		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
 		if (newTexture == NULL)
 		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+			std::cout << "Unable to create texture from " << path.c_str() << "! SDL Error: " << SDL_GetError() << std::endl;
 		}
 
 		//Get rid of old loaded surface
@@ -48,88 +125,58 @@ SDL_Texture* loadTexture(std::string path)
 	return newTexture;
 }
 
-int main(int argc, char* args[]) {
-
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0) {
-		OutputDebugString("SDL Could not be initialized, exiting\n");
-		exit(1);
-	}
-
-	//Set texture filtering to linear
-	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
-		OutputDebugString("Warning: Linear texture filtering not enabled!\n");
-	}
-
-	// Create window
-	// SDL_WINDOW_SHOWN flag is ignored according to document.
-	// Might want to eventually use SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_INPUT_GRABBED
-	// https://wiki.libsdl.org/SDL_CreateWindow
-	gWindow = SDL_CreateWindow("A cool title",
-								SDL_WINDOWPOS_CENTERED,
-								SDL_WINDOWPOS_CENTERED,
-								SCREEN_WIDTH,
-								SCREEN_HEIGHT,
-								SDL_WINDOW_SHOWN); 
-	if (gWindow == NULL) {
-		OutputDebugString("Window could not be created!\n");
-		exit(1);
-	}
-
-	//Create renderer for window
-	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-	if (gRenderer == NULL) {
-		OutputDebugString("Renderer could not be created!\n");
-		exit(1);
-	}
-
-	//Initialize renderer color (renderer, red, green, blue, alpha)
-	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-	// Requires another library 'SDL_Image'
-	int imgFlags = IMG_INIT_PNG;
-	if (!(IMG_Init(imgFlags) & imgFlags)) {
-		OutputDebugString("\n\n\n");
-		OutputDebugString(IMG_GetError());
-		OutputDebugString("\n\n\n");
-		exit(2);
-	}
-
-	//Load PNG texture
-	gTexture = loadTexture("texture.png");
-	if (gTexture == NULL)
-	{
-		OutputDebugString("Failed to load texture image!\n");
-		exit(1);
-	}
-
-	//Main loop flag
-	bool quit = false;
-
-	//Event handler
-	SDL_Event e;
-
+void Game::run()
+{
 	//While application is running
 	while (!quit)
 	{
+		//Event handler
+		SDL_Event e;
+
 		//Handle events on queue
 		while (SDL_PollEvent(&e) != 0)
 		{
-			//User requests quit
-			if (e.type == SDL_QUIT)
+			switch(e.type)
 			{
-				quit = true;
+			case SDL_QUIT:
+				stop();
+				break;
+			case SDL_KEYDOWN:
+				handleInput(e);
+				break;
 			}
 		}
 
 		//Clear screen
-		SDL_RenderClear(gRenderer);
+		SDL_RenderClear(renderer);
 
 		//Render texture to screen
-		SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
 
 		//Update screen
-		SDL_RenderPresent(gRenderer);
+		SDL_RenderPresent(renderer);
 	}
+}
+
+void Game::handleInput(SDL_Event e)
+{
+	switch(e.key.keysym.sym)
+	{
+	case SDLK_ESCAPE:
+		stop();
+		break;
+	}
+	return;
+}
+
+void Game::stop()
+{
+	quit = true;
+}
+
+int main(int argc, char* args[]) {
+	Game game;
+	game.run();
 
 	return 0;
 }
